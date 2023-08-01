ebccd820468c0b15ae1155c1bd4d624b7bf3fd09
Mahmud Mai <mahmudx00@gmail.com>
2023-08-01 16:06:34 +0100

    modify bar chart heading

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,4 +1,4 @@
-// import { BsChevronDown } from "react-icons/bs";
+import { BsChevronDown } from "react-icons/bs";
 // import { LuPieChart } from "react-icons/lu";
 
 import SummaryBarChart from "../components/SummaryBarChart";
@@ -9,8 +9,8 @@ import MarketingPieChart from "../components/MarketingPieChart";
 const Dashboard = () => {
   return (
     <div className="mx-[19px] mt-[112px] mb-5">
-      {/* Summary Cars  */}
-      <div className="grid grid-flow-col gap-4">
+      {/* Row One: Statistics Summary  */}
+      <div className="grid grid-cols-5 gap-4">
         <div className="col-span-3">
           <div className="grid grid-cols-2 gap-4">
             <div>
@@ -26,8 +26,8 @@ const Dashboard = () => {
         </div>
       </div>
 
-      {/* Charts And Recent Orders  */}
-      <div className="grid grid-flow-col gap-4 mt-5">
+      {/* Row two: Charts And Recent Orders  */}
+      <div className="grid grid-cols-5 gap-4 mt-5">
         <div className="col-span-3">
           <div className="row-span-1 grid grid-cols-2 gap-4">
             <div className="col-span-1 bg-white">
@@ -42,12 +42,36 @@ const Dashboard = () => {
               </p>
             </div>
           </div>
-          <div className="row-span-1 grid grid-cols-1 bg-white px-5  mx-auto mt-5">
-            Marketing
-            <SummaryBarChart />
+          <div className="row-span-1 grid grid-cols-1 bg-white px-5 mx-auto mt-5">
+            <div className="px-5 py-[15px]">
+              {/* Heading  */}
+              <div className="flex justify-between">
+                {/* Summary & This Week  */}
+                <div className="flex justify-between gap-[21px] items-center">
+                  <h2>Summary</h2>
+                  <div className="flex gap-5 items-center text-3 bg-[#5570F114] text-primary-100 font-normal px-3">
+                    This Week
+                    <span>
+                      <BsChevronDown />
+                    </span>
+                  </div>
+                </div>
+                {/* 7 days section  */}
+                <div className="flex gap-5 items-center text-3 text-[#1C1D22] font-normal px-3">
+                  7 days
+                  <span>
+                    <BsChevronDown />
+                  </span>
+                </div>
+              </div>
+              {/* Bar Chart */}
+              <div className="mt-9">
+                <SummaryBarChart />
+              </div>
+            </div>
           </div>
         </div>
-        <div className=" col-span-2 row-span-2">
+        <div className="col-span-2 row-span-2">
           <OrdersList />
         </div>
       </div>
